8c0327279ec9ec81cd3bf394de54445acd6d9d97
SamilOzer <samil_ozer@hotmail.com>
2026-08-05 22:24:12 +0300

    fix: remove .next folder from git tracking

--- .next/server/chunks/ssr/_next-internal_server_app_ansiklopedi_page_actions_0e0s4a1.js
+++
@@ -1,3 +0,0 @@
-module.exports=[42156,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_ansiklopedi_page_actions_0e0s4a1.js.map
--- .next/server/chunks/ssr/_next-internal_server_app_katkida-bulun_page_actions_19xci28.js
+++
@@ -1,3 +0,0 @@
-module.exports=[42363,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_katkida-bulun_page_actions_19xci28.js.map
